73a9727e28b738504358ed2c73fa05edc83573ee
boybutcher <BYRONdeLARGE@yahoo.com>
2018-04-10 15:25:50 -0400

    generic click handler

--- scavenger-client/src/components/NavBar/NavBar.js
+++ scavenger-client/src/components/NavBar/NavBar.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import './NavBar.css';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.navClickHandler = this.navClickHandler.bind(this);
+  }
+
+  navClickHandler() {
+    console.log('button clicked!');
+  }
+
   render() {
     return (
       <div className='nav-bar'>
-        <div className='home-button'>scavenger</div>
-        <div className='nav-button'>browse</div>
-        <div className='nav-button'>events</div>
-        <div className='nav-button'>host</div>
+        <div 
+          className='home-button'
+          onClick={this.navClickHandler}
+        >
+          scavenger
+        </div>
+        <div 
+          className='nav-button'
+          onClick={this.navClickHandler}
+        >
+          browse
+        </div>
+        <div 
+          className='nav-button'
+          onClick={this.navClickHandler}
+        >
+          events
+        </div>
+        <div 
+          className='nav-button'
+          onClick={this.navClickHandler}
+        >
+          host
+        </div>
       </div>
     )
   }
