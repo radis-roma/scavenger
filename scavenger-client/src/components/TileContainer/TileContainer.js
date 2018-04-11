af53c5dfa0a26904455424e14fcce62a64af3679
boybutcher <BYRONdeLARGE@yahoo.com>
2018-04-11 16:05:30 -0400

    dummy data for game lobbies.

--- scavenger-client/src/components/TileContainer/TileContainer.js
+++ scavenger-client/src/components/TileContainer/TileContainer.js
@@ -6,7 +6,39 @@ class TileContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gameLobbies: [],
+      gameCollection: [
+        {
+          name: 'Warcraft III: The Frozen Throne',
+          lobbyList: [
+            {
+              hostName: 'boybutcher',
+              region: 'North America',
+              playerCount: 1,
+              playerCap: 8,
+              description: 'let\'s play Island Troll Tribes!\nThe lobby name is \'cats\'\n the password is \'1234\'',
+            },
+            {
+              hostName: 'braunstein',
+              region: 'North America',
+              playerCount: 1,
+              playerCap: 2,
+              description: '1v1 me on Booty Bay.'
+            }
+          ],
+        },
+        {
+          name: 'Halo: Reach',
+          lobbyList:[
+            {
+              hostName: 'foosh',
+              region: 'East Asia',
+              playerCount: 1,
+              playerCap: 16,
+              description: 'CTF on Bloodgulch'
+            }
+          ]
+        }
+      ],
     }
   }
 
