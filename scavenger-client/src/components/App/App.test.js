e18358214cbf695070d61c21dc0c5b1bcf636c44
boybutcher <BYRONdeLARGE@yahoo.com>
2018-04-12 14:31:15 -0400

    formatting. Implement GameTile test

--- scavenger-client/src/components/App/App.test.js
+++ scavenger-client/src/components/App/App.test.js
@@ -4,6 +4,7 @@ import App from './App.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
